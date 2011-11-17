e0182b4225bfdb55beb1e5ba17e58d3ecb49589c
Jonathan <ksk.logan@hotmail.com>
2011-11-17 15:13:55 -0800

    Finished problem 2.

--- assignment-6/stackinspect.rkt
+++ assignment-6/stackinspect.rkt
@@ -1,10 +1,10 @@
 #lang plai
 
-;; Name 1: 
-;; Student ID 1:
+;; Name 1: Jonathan Neufeld
+;; Student ID 1: 30671093
 
-;; Name 2: 
-;; Student ID 2:
+;; By default, a stack frame is not blessed.
+(define is-blessed (box false))
 
 (define-type KCFAE
   [num (n number?)]
@@ -27,56 +27,95 @@
 ;; lookup : symbol DefrdSub -> KCFAE-Value
 (define (lookup name ds)
   (type-case DefrdSub ds
-             [mtSub () (error 'lookup "no binding for identifier")]
-             [aSub (bound-name bound-value rest-ds)
-                   (if (symbol=? bound-name name)
-                       bound-value
-                       (lookup name rest-ds))]))
+    [mtSub () (error 'lookup "no binding for identifier")]
+    [aSub (bound-name bound-value rest-ds)
+          (if (symbol=? bound-name name)
+              bound-value
+              (lookup name rest-ds))]))
 
 ;; num+ : KCFAE-Value KCFAE-Value -> KCFAE-Value
 (define (num+ x y)
   (numV (+ (type-case KCFAE-Value x
-                      [numV (n) n]
-                      [else (error 'num+ "addition of a non-number")])
+             [numV (n) n]
+             [else (error 'num+ "addition of a non-number")])
            (type-case KCFAE-Value y
-                      [numV (n) n]
-                      [else (error 'num+ "addition of a non-number")]))))
+             [numV (n) n]
+             [else (error 'num+ "addition of a non-number")]))))
+
 ;; num-zero? : KCFAE-Value -> boolean
 (define (num-zero? n)
   (type-case KCFAE-Value n
-                [numV (n) (= n 0)]
-                [else (error 'num-zero? "num-zero? argumnet must be a number")]))
-
+    [numV (n) (= n 0)]
+    [else (error 'num-zero? "num-zero? argument must be a number")]))
 
 ;; interp : KCFAE Env receiver -> (doesn't return)
 (define (interp expr env k)
   (type-case KCFAE expr
-             [num (n) (k (numV n))]
-             [add (l r) (interp l env
-                                (lambda (lv)
-                                  (interp r env
-                                          (lambda (rv)
-                                            (k (num+ lv rv))))))]
-             [if0 (test truth falsity)
-                  (interp test env
-                          (lambda (tv)
-                            (if (num-zero? tv)
-                                (interp truth env k)
-                                (interp falsity env k))))]
-             [id (v) (k (lookup v env))]
-             [fun (param body)
-                  (k (closureV (lambda (arg-val dyn-k)
-                                 (interp body (aSub param arg-val env) dyn-k))))]
-             [app (fun-expr arg-expr)
-                  (interp fun-expr env
-                          (lambda (fun-val)
-                            (interp arg-expr env
-                                    (lambda (arg-val)
-                                      (type-case KCFAE-Value fun-val
-                                                 [closureV (c) (c arg-val k)]
-                                                 [else (error "not an applicable value")])))))]
-	     [else (error 'interp "incomplete interp!")]))
+    [num (n) (k (numV n))]
+    [add (l r) (interp l env
+                       (lambda (lv)
+                         (interp r env
+                                 (lambda (rv)
+                                   (k (num+ lv rv))))))]
+    [if0 (test truth falsity)
+         (interp test env
+                 (lambda (tv)
+                   (if (num-zero? tv)
+                       (interp truth env k)
+                       (interp falsity env k))))]
+    [id (v) (k (lookup v env))]
+    [fun (param body)
+         (k (closureV (lambda (arg-val dyn-k)
+                        (interp body (aSub param arg-val env) dyn-k))))]
+    [app (fun-expr arg-expr)
+         (interp fun-expr env
+                 (lambda (fun-val)
+                   (interp arg-expr env
+                           (lambda (arg-val)
+                             (type-case KCFAE-Value fun-val
+                               [closureV (c) (c arg-val k)]
+                               [else (error "not an applicable value")])))))]
+    [bless (body) (begin
+                    (set! is-blessed (box true))
+                    (interp body env (lambda (m) (begin
+                                                   (set! is-blessed (box false))
+                                                   (k m))))
+                    (display "Should never print this..."))]
+    [check () (if (unbox is-blessed) 
+                  (k (numV 0)) 
+                  (error 'check "Stack frame not blessed."))] ))
 
 ;; test-interp : KCFAE -> KCFAE-Value
 (define (test-interp expr)
   (let/cc k (interp expr (mtSub) k)))
+
+;; tests:
+(display "Just bless:\n")
+(test (test-interp (bless (num 5))) (numV 5))
+(test (test-interp (app (bless (fun 'x (add (id 'x) (id 'x)))) (num 5))) (numV 10))
+(test (test-interp (add (add (num 10) (num 2)) (add (num 20) (num 2)))) (numV 34))
+
+(display "\nJust check:\n")
+(test/exn (test-interp (check)) "")
+(test/exn (test-interp (if0 (check) (num 10) (num 20))) "")
+(test/exn (test-interp (app (fun 'f (if0 (check)
+                                         (app (id 'f) (num 2))
+                                         (num 0))) (fun 'x (add (id 'x) (id 'x))))) "")
+
+(display "\nBless and check:\n")
+(test (test-interp (bless (if0 (check) (num 5) (num 0)))) (numV 5))
+(test (test-interp (app (fun 'f (bless (if0 (check) (app (id 'f) (num 2)) (num 0)))) (fun 'x (add (id 'x) (id 'x))))) (numV 4))
+(test (test-interp (add (bless (if0 (check)
+                                    (add (num 10) (num 2))
+                                    (num 0)))
+                        (bless (if0 (check)
+                                    (add (num 20) (num 2))
+                                    (num 0)))))
+      (numV 34))
+(test/exn (test-interp (add (bless (if0 (check)
+                                    (add (num 10) (num 2))
+                                    (num 0)))
+                        (if0 (check)
+                                    (add (num 20) (num 2))
+                                    (num 0))))
+      "")
